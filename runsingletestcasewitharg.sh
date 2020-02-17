130b66fade0800b92ffb8d007bbf271ce1be4cec
Abhishek <abhialva0612@gmail.com>
2020-02-17 14:27:09 +0530

    this is initial commit

---
+++ runsingletestcasewitharg.sh
@@ -0,0 +1,25 @@
+
+
+#Run Single test case
+
+#java -cp singletestcase.jar com.IIT.pageObjects.ExecuteLeadTestSingleTestCaseRunner
+#change the name withing quotes with the test case name
+
+
+ #"customWords", "AcademicYear", "StudentCategory",
+#"HrSettings", "EmployeeAdmission", "Program and Stream creation", "Create_Course_Category",
+#"addCourses", "VerifyAndApproveCurricula1",	"StudentAdmission", "AcademicSemester",
+#"Cohort", "CohortUpload", "CoursesInCurrentSemester-Verify&Approve",
+#"WindowFrame-PreRequisite", "BacklogStudents-FileUpload, Import Logs, Scheduled Jobs",
+#"BacklogStudents-Bulk EditNavigation", "PreRegistrationAdminNStudent-Submit Registration Form",
+#"WindowFrame-PreRequisite", "SemesterEnrollment- COTnFAApproval", "WindowFrame-PreRequisite", 
+#"SemesterEnrollment-DropApproval", "WindowFrame-PreRequisite", "Student'sAddDrop-Interface",
+#"WindowFrame-PreRequisite", "Student'sDrop- Interface", "Course_Records"		
+
+
+
+#Change testcase name between double quotes
+java -cp singletestcaseinputargument.jar com.IIT.pageObjects.ExecuteLeadTestSingleTestCaseRunner "CustomWords"
+
+
+
---
+++ runthejar.sh
@@ -0,0 +1,13 @@
+
+#Run all test cases at once
+
+#java -cp alltestcasesatonce.jar com.IIT.pageObjects.ExecuteLeadTest
+
+#Run Single test case
+
+#java -cp singletestcase.jar com.IIT.pageObjects.ExecuteLeadTestSingleTestCaseRunner
+
+
+#Run the jar
+
+java -cp singletestcaseinputargument.jar
